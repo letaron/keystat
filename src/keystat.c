3e756255d3c3818c829042f1218e5b73398a1abe
Thomas Martin <thomas@oopss.org>
2016-11-09 20:32:38 +0100

    Remove keyboard interrupt handler

--- src/keystat.c
+++ src/keystat.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>    // included for all kernel modules
 #include <linux/kernel.h>    // included for KERN_INFO
 #include <linux/init.h>      // included for __init and __exit macros
+#include <linux/interrupt.h> // included for free_irq
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Le Taron");
@@ -9,6 +10,7 @@ MODULE_DESCRIPTION("A keystroke stats counter");
 static int __init hello_init(void)
 {
     printk(KERN_INFO "Keystat counter enabled!\n");
+    free_irq(1, NULL);
     return 0;    // Non-zero return means that the module couldn't be loaded.
 }
 
